c87180fb8f9b860c61e13027ee4e5676f962face
Zahra Suleymanova <105732760+Zahra46031@users.noreply.github.com>
2024-07-18 22:00:05 -0400

    quick fix to roll dice

--- public/roll_dice.php
+++ public/roll_dice.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-if ($_SESSION['rolls_left'] > 0) {
+if ($_SESSION['rolls'] > 0) {
     for ($i = 0; $i < 5; $i++) {
         if ($_SESSION['dice'][$i] === 0) {
             $_SESSION['dice'][$i] = rand(1, 6);
         }
     }
-    $_SESSION['rolls_left']--;
+    $_SESSION['rolls']--;
 }
 
 echo json_encode($_SESSION['game_state']);
