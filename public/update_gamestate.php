f6f653f2b77378aef377c18106b8f11460feecd4
Zahra Suleymanova <105732760+Zahra46031@users.noreply.github.com>
2024-07-18 22:38:40 -0400

    update gamestate fixed

--- public/update_gamestate.php
+++ public/update_gamestate.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
+require 'YatzyEngine.php';
+
+header('Content-Type: application/json');
 
 
 if (!isset($_SESSION['game_state'])) {
+  
     $_SESSION['game_state'] = [
     'dice' => [0, 0, 0, 0, 0],
     'rolls' => 0,
@@ -12,6 +16,43 @@ if (!isset($_SESSION['game_state'])) {
     ];
 }
 
+$gameState = $_SESSION['game_state'];
 
-echo json_encode($_SESSION['game_state']);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $input = json_decode(file_get_contents('php://input'), true);
+    
+    if (isset($input['action'])) {
+        $action = $input['action'];
+
+        switch ($action) {
+            case 'roll_dice':
+                if ($gameState['rolls'] < 3) {
+                    $gameState['dice'] = YatzyEngine::rollDice();
+                    $gameState['rolls']++;
+                }
+                break;
+            case 'score_category':
+                if (isset($input['category'])) {
+                    $category = $input['category'];
+                    if ($gameState['scores'][$category] === null) {
+                        $score = YatzyEngine::calculateScore($category, $gameState['dice']);
+                        $gameState['scores'][$category] = $score;
+                        $gameState['total_score'] += $score;
+                        
+                        $gameState['rolls'] = 0;
+                        $gameState['dice'] = [0, 0, 0, 0, 0];
+                    }
+                }
+                break;
+     
+        }
+    }
+}
+
+
+$_SESSION['game_state'] = $gameState;
+
+
+echo json_encode($gameState);
 ?>
