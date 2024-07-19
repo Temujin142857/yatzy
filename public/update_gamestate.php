09c4b11df23fedf73d2393e22795ffd2196988f0
Tomio <temujin142857@gmail.com>
2024-07-18 22:22:26 -0400

    fixed syntax in gamestate, correcty update data in ui

--- public/update_gamestate.php
+++ public/update_gamestate.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['game_state'])) {
     'rolls' => 0,
     'scores' => array_fill_keys(['ones', 'twos', 'threes', 'fours', 'fives', 'sixes', 'three_of_a_kind', 'four_of_a_kind', 'full_house', 'small_straight', 'large_straight', 'chance', 'yatzy'], null),
     'total_score' => 0,
-    'diceKeep' => array(false,false,false,false,false);
+    'diceKeep' => array(false,false,false,false,false)
     ];
 }
 
